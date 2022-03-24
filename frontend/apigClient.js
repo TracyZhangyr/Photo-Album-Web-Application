1d5c94410b7845c15e1cff8db6a82bcd9bb0e9cd
Jincheng Xu <jx2467@columbia.edu>
2022-03-24 16:40:23 -0400

    Add frontend code

--- frontend/apigClient.js
+++ frontend/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://zypunyr0tc.execute-api.us-east-1.amazonaws.com/test-stage';
+    var invokeUrl = 'https://zypunyr0tc.execute-api.us-east-1.amazonaws.com/dev-stage';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
